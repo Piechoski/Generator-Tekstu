2c129ed96034df186987da1b3dec5cf5ffd5ca0f
kamil <kamil.piech.91@wp.pl>
2017-09-02 11:07:34 +0200

    generator Lorem Ipsum

--- src/main/java/pl/akademiakodu/loremipsum2/LoremIpsumController.java
+++ src/main/java/pl/akademiakodu/loremipsum2/LoremIpsumController.java
@@ -1,14 +1,25 @@
 package pl.akademiakodu.loremipsum2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class LoremIpsumController {
     @GetMapping("/")
-    public String home(){
+    public String home(ModelMap modelMap){
+        modelMap.addAttribute("text","t" );
         return "home";
 
+    }
+
+    @PostMapping("/generate")
+    public String generate(@ModelAttribute Form form, ModelMap modelMap ){
+        modelMap.addAttribute("text", "dupa");
+        return "home";
     }
 }
